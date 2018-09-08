4295c4ce5876479acfc203073f592228e7d68bba
jill zhang <talktojillzhang@gmail.com>
2018-09-08 02:53:59 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,11 @@
 def turn_count(board)
+  count =0
   for element in board
     if element != '' || element != " "
-      count = count
+      count += count
+    end
+  end
+  return count
 end
 
 def current_player()
